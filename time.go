a12b2d7643b9ba91f37ccb4be9c5727fd360bd65
Lisha Middha <62130189+lishamidha@users.noreply.github.com>
2021-09-25 14:40:25 +0530

    Add files via upload

---
+++ time.go
@@ -0,0 +1,14 @@
+//WAP to print the current time
+
+package main
+
+import (
+	"fmt"
+	"time" //package used to use time and related function in program
+)
+
+func main() {
+
+	fmt.Println(time.Now())
+
+}
